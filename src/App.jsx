5a5ab3822e9cc68fc04f4f41c2b051afc883a402
ayoub <bouraissiayoub1@gmail.com>
2026-03-26 15:15:40 +0100

    affichage du table  sans chargement dynamique

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import './App.css'
 import DataTable from './Component/DataTable/DataTable.jsx';
 
 function App() {
+    const colonnes = [
+        { label: "id", key: "id" },
+        { label: "prenom", key: "firstName" },
+        { label: "nom", key: "lastName" },
+        { label: "email", key: "email" },
+    ];
 
-  return (
-    <>
-      <DataTable columns={[]} fetchData={() => {}} />
-    </>
-  );
+    const fetchUsers = () => {
+        return fetch("https://dummyjson.com/users")
+            .then(result => result.json())
+            .then(data => data.users);
+    };
+
+    return (
+        <div className="p-4">
+            <h1 className='text-3xl'>tableau des utilisateur</h1>
+            <DataTable colonnes={colonnes} fetchData={fetchUsers} />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/Component/DataTable/DataTable.jsx
+++ src/Component/DataTable/DataTable.jsx
@@ -1,9 +1,43 @@
-import "./DataTable.css";
+import { useEffect, useState } from "react";
 
-export default function DataTable(){
-  return(
-    <div>
-      
-    </div>
-  )
+export default function DataTable({ colonnes, fetchData }) {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchData()
+            .then(result => {
+               console.log(result);
+                setData(result);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <p className="p-4 text-sm">loading</p>;
+    }
+    
+      return (
+        <div className="p-4 ">
+            <table className="w-full bg-white">
+              <tr className="bg-black text-white">
+                  {colonnes.map((colonne, index) => (
+                    <td key={index} className="p-2">
+                        {colonne.label}
+                    </td>
+                ))}
+              </tr>
+
+              {data.map((item, index) => (
+                  <tr key={index} className="border-b">
+                      {colonnes.map((colonne, i) => (
+                         <td key={i} className="p-2">
+                             {item[colonne.key]}
+                        </td>
+                    ))}
+                </tr>
+            ))}
+            </table>
+          </div>
+    );
 }
